f7a4c8252ba015eeca4c4cea23fecb6470567ef3
Yaelle Marcilly <yalloumcy@gmail.com>
2022-10-20 20:54:50 +0200

    p1 from yaelle working, vagrant up twice. first is needed to install plugin and confirm it, second to launc vms

--- p1/scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-# this script is not run because it does not make sense to download 500M and
-# unpack it twice
-
-yum update -y && yum install -y net-tools && curl -sfL https://get.k3s.io | sh -
-# todo: ajouter les guest additions dans la 0.1.1
-
-# It was added to https://app.vagrantup.com/mehdee/boxes/k3s_one
-# Vagrant directly pulls it, with the configuration files in ../confs
-# Commands to setup the box in the Vagrant cloud, are detailed in notes.md
---
+++ p1/scripts/sw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "WORKER PART"
+export K3S_KUBECONFIG_MODE="644"
+export K3S_URL="https://192.168.42.110:6443"
+export K3S_TOKEN=`cat /vagrant/k3s_token`
+curl -sfL https://get.k3s.io | sh -s - agent --node-ip 192.168.42.111

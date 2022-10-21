68b7a5162ab382397d5b101d74e5ac80632c1700
Yaelle Marcilly <yalloumcy@gmail.com>
2022-10-21 22:31:39 +0200

    p1: changed name for ymarcill, clean scripts

--- p1/scripts/sw.sh
+++ p1/scripts/sw.sh
@@ -4,3 +4,5 @@ export K3S_KUBECONFIG_MODE="644"
 export K3S_URL="https://192.168.42.110:6443"
 export K3S_TOKEN=`cat /vagrant/k3s_token`
 curl -sfL https://get.k3s.io | sh -s - agent --node-ip 192.168.42.111
+echo "NETWORK INTERFACES: ip a"
+ip a
